7cd92306062b77c1787477c2623554c62785f87d
Lucas Leonardo <llbs@cin.ufpe.br>
2022-06-01 08:34:53 -0300

    IO example using bufio/ioutil to count line occurrences in input

---
+++ ch1/dup1.go
@@ -0,0 +1,25 @@
+// dup1 show the text and your respective count of all line
+// thats appear one more time in input
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
---
+++ ch1/dup2.go
@@ -0,0 +1,44 @@
+// Dup1 show the text and your respective count of all line thats appear
+// one more time in input. It read from stdin or a file named list.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
